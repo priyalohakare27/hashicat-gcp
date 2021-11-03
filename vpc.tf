e1b571140947d3ffd33a5d232d9b471fc0083437
Priya <priyalohakare27@gmail.com>
2021-11-03 18:25:33 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -3,11 +3,11 @@ module "network" {
     version = "3.4.0"
 
     project_id   = var.project
-    network_name = "gaurav-network"
+    network_name = "priya-network"
 
     subnets = [
         {
-            subnet_name   = "gaurav-subnet"
+            subnet_name   = "priya-subnet"
             subnet_ip     = "10.100.10.0/24"
             subnet_region = var.region
         }
